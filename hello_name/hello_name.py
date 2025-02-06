fb4ede8ba5c62fe070e42da1ed50c03606f01842
Maham Bhatti <maham.bhatti@samsung.com>
2025-02-06 15:55:40 -0500

    updated hello_name

--- hello_name/hello_name.py
+++ hello_name/hello_name.py
@@ -2,5 +2,4 @@ try:
     username =input("Enter your name: ")
     print("Hello : " + username + "! Have a great day! ")
 except EOFError:   
-    name = "mary"
-print("No input recieved! ")
+    print("No input recieved! ")
